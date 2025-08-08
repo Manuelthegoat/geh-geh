736924353cac6718b315f2f47996c9c2c47b3c4d
manuelthegoat <uwadiegwu.emmanuel48@gmail.com>
2025-08-08 20:20:36 +0100

    gg

--- src/app/quiz/page.tsx
+++ src/app/quiz/page.tsx
@@ -69,7 +69,6 @@ export default function QuizPage() {
             Retake Quiz
           </button>
 
-          {/* Text inside card */}
           <p
             style={{
               marginTop: "1.5rem",
